58493f694c4c366cede4fb87f2da99e8cfb80eca
victorAmadeu <victoramadeusm@hotmail.com>
2024-09-07 14:46:36 -0300

    Update dados_plantas_Rio.js
    Ajeitando caminho das imagens

--- dados_plantas_Rio.js
+++ dados_plantas_Rio.js
@@ -7,7 +7,7 @@ let plantasToxicas = [
         principioAtivo: "óleos voláteis, felandreno, carvacrol e pineno",
         parteToxica: "todas as partes da planta.",
         sintomas: "o contato ou, possivelmente, a proximidade provoca reação dérmica local (bolhas, vermelhidão e coceira), que persiste por vários dias; a ingestão pode provocar manifestações gastrintestinais.",
-        imagem: "images/Aroeira.jpg",
+        imagem: "Aroeira.jpg",
         tags: "aguara-iba, aguara iba, aroeira branca, balsamo, cabui, cambui, coracao de bugre, fruto de raposa",
         link: "https://pt.wikipedia.org/wiki/Aroeira"
 
@@ -20,7 +20,7 @@ let plantasToxicas = [
         principioAtivo: "cristais de oxalato de cálcio",
         parteToxica: "todas as partes da planta.",
         sintomas: "a ingestão e o contato podem causar sensação de queimação, edema (inchaço) de lábios, boca e língua, náuseas, vômitos, diarréia, salivação abundante, dificuldade de engolir e asfixia; o contato com os olhos pode provocar irritação e lesão da córnea.",
-        imagem: "images/jiboia-fio-cruz.jpg",
+        imagem: "jiboia-fio-cruz.jpg",
         tags: "jiboia verde",
         link: "https://pt.wikipedia.org/wiki/Epipremnum_aureum"
 
@@ -33,7 +33,7 @@ let plantasToxicas = [
         principioAtivo: "solanina e outros glicoalcalóides",
         parteToxica: "frutos verdes e folhas.",
         sintomas: "a ingestão de frutos verdes e folhas pode causar distúrbios gastrointestinais como vômitos, diarreia, e em casos mais graves, depressão do sistema nervoso central e paralisia.",
-        imagem: "images/Tinhorao.jpg",
+        imagem: "Tinhorao.jpg",
         tags: "erva de rato, tomate bravo, Tinhorao",
         link: "https://pt.wikipedia.org/wiki/Tinhor%C3%A3o"
     },
@@ -45,7 +45,7 @@ let plantasToxicas = [
         principioAtivo: "cristais de oxalato de cálcio",
         parteToxica: "todas as partes da planta.",
         sintomas: "o contato com a planta pode causar irritação na pele e nas mucosas, dor e inchaço. A ingestão pode causar inchaço na boca e garganta, dificuldade para engolir e, em casos mais graves, problemas respiratórios.",
-        imagem: "images/Anturio.jpg",
+        imagem: "Anturio.jpg",
         tags: "flor de flamingo, Anturio",
         link: "https://pt.wikipedia.org/wiki/Anthurium"
     },
@@ -57,7 +57,7 @@ let plantasToxicas = [
         principioAtivo: "cristais de oxalato de cálcio, saponinas",
         parteToxica: "todas as partes da planta.",
         sintomas: "a ingestão e o contato podem causar sensação de queimação, edema (inchaço) de lábios, boca e língua, náuseas, vômitos, diarréia, salivação abundante, dificuldade de engolir e asfixia; o contato com os olhos pode provocar irritação e lesão da córnea.",
-        imagem: "images/Comigo-ninguEm-pode.jpg",
+        imagem: "Comigo-ninguEm-pode.jpg",
         tags: "aninga-do-Para, aninga do Para pará, Comigo ninguem pode, comigo ninguém pode",
         link: "https://pt.wikipedia.org/wiki/Dieffenbachia_seguine"
 
@@ -70,8 +70,8 @@ let plantasToxicas = [
         principioAtivo: "glicosídeos cardiotóxicos",
         parteToxica: "todas as partes da planta.",
         sintomas: "a ingestão ou o contato com o látex podem causar dor em queimação na boca, salivação, náuseas, vômitos intensos, cólicas abdominais, diarréia.",
-        imagem: "images/Espirradeira.jpg",
-        imagem2: "images/Espirradeira2.jpg",
+        imagem: "Espirradeira.jpg",
+        imagem2: "Espirradeira2.jpg",
         tags: "louro-rosa",
         link: "https://pt.wikipedia.org/wiki/Oleandro"
     },
@@ -83,7 +83,7 @@ let plantasToxicas = [
         principioAtivo: "oxalato de cálcio, saponinas e substâncias hipersensibilizantes",
         parteToxica: "todas as partes da planta.",
         sintomas: "a ingestão causa uma queimação excessiva. Em contato com a pele, causa irritação.",
-        imagem: "images/Espada-de-SJorge.jpeg",
+        imagem: "Espada-de-SJorge.jpeg",
         tags: "Espada-de-Sao-Jorge, Espada-de-São-Jorge, espada de sao jorge, Espada-de, rabo lagarto, língua lingua de sogra.",
         link: "https://pt.wikipedia.org/wiki/Dracaena_trifasciata"
     },
@@ -95,7 +95,7 @@ let plantasToxicas = [
         principioAtivo: "diterpenos derivados do andromedotoxina, de ação cardiotóxica",
         parteToxica: "todas as partes da planta.",
         sintomas: "salivação, vômitos, lacrimação, rinorréia (corrimento de mucosidades do nariz). Em casos graves pode ocorrer hipotensão, dor de cabeça, bradicardia, convulsões e coma.",
-        imagem: "images/Azaleia.jpg",
+        imagem: "Azaleia.jpg",
         tags: "azaleia, azaleia anã ana, azaleia-da-california, da california,  loureiro-branco, grande",
         link: "https://pt.wikipedia.org/wiki/Az%C3%A1lea"
     },
@@ -107,7 +107,7 @@ let plantasToxicas = [
         principioAtivo: "glicósidos cardiotônicos",
         parteToxica: "todas as partes da planta, particularmente a seiva.",
         sintomas: "a ingestão de qualquer parte da planta, particularmente o látex resinoso, quando ingerida provoca distúrbios gastrintestinais: vômitos frequentes, alterações cardiovasculares caracterizadas por arritmias, náuseas, cólicas abdominais e, em doses maiores, pode ser fatal.",
-        imagem: "images/Alamanda.jpg",
+        imagem: "Alamanda.jpg",
         tags: "dedeira de moça, amarela, trombeta ouro, dedal de dama, dedal-de-dama",
         link: "https://pt.wikipedia.org/wiki/Alamanda"
             
